e97ac8b21924f761f54873bd566ea954af95d52d
Osman Seixas <osmanseixas@gmail.com>
2025-03-30 00:21:39 -0400

    refatorando a tela de categorias

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -6,7 +6,7 @@ export const categoriesMock = [
     { id: 5, descricao: "Educação", icone: "FaBook", cor: "#2497C9" },
     { id: 6, descricao: "Trabalho", icone: "FaPeopleCarry", cor: "#7E88CD" },
     { id: 7, descricao: "Serviço", icone: "IoConstruct", cor: "#E58E18" },
-    { id: 8, descricao: "Moradia", icone: "FaHouseChimney", cor: "#B66896" },
+    { id: 8, descricao: "Habitação", icone: "FaHouseChimney", cor: "#B66896" },
     { id: 9, descricao: "Alimentação", icone: "MdFastfood", cor: "#9DD3AF" },
     { id: 10, descricao: "Empréstimo", icone: "FaMoneyBillTransfer", cor: "#F07A9E" },
     { id: 11, descricao: "Entretenimento", icone: "FaMasksTheater", cor: "#F8BDBD" },
